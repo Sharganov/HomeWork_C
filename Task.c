df6d4e2cc328ed346875b2510a9ede97783816d2
Sharganov Artem <sharganov.artem@yandex.ru>
2014-10-28 23:38:11 +0400

    Create Task.c

---
+++ Task.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+union 
+{
+	struct 
+	{
+	int mant: 23;
+	unsigned int exp: 8;
+	int sign: 1;
+	} BitField;
+	float f;
+} str;
+
+int main() 
+{	
+	scanf("%f",&str.f);
+	if(str.BitField.exp == 0 && str.BitField.mant == 0) 
+	{
+			printf("zero");
+	}
+	else if (str.BitField.sign == 0 &&  str.BitField.exp == 255 && str.BitField.mant==0 ) 
+	{
+		printf(" + infinity ");
+	}
+	else if (str.BitField.sign == -1 &&  str.BitField.exp == 255 && str.BitField.mant==0)
+	{
+		printf(" - infinity ");
+	} 
+	else if (str.BitField.exp == 255 && str.BitField.mant != 0 )
+	{
+		printf("NuN");
+	}
+	else
+	{
+	printf ("(-1) ^ %d * 2^ %d * %f = %f", str.BitField.sign, str.BitField.exp - 127, 1+ str.BitField.mant / (float) (1 << 23), str.f);
+	}
+	return 0;
+}
