646abedbdc375edc3248f1db6557d985d81657a0
Sharganov Artem <sharganov.artem@yandex.ru>
2014-10-29 16:00:17 +0400

    Update Task5_2.c

--- Task5_2.c
+++ Task5_2.c
@@ -1,5 +1,8 @@
+/*Welcome to Storage
+ Allocation of sign, exponent, significand in float number with using bitfield
+ By Sharganov Artyom */
+ 
 #include <stdio.h>
-
 union
 {
 float f;
@@ -8,25 +11,25 @@ int i;
 
 int main()
 {
-	scanf("%f",&gen.f);
+	scanf ("%f", &gen.f);
 	unsigned int mantisa  = (gen.i & 0x003fffff);
 	unsigned int exp = (gen.i & 0x7f800000) >> 23;
 	int sign  = gen.i & 0x100000000;
-		if(exp == 0 &&  mantisa == 0) 
+		if(exp == 0 && mantisa == 0) 
 	{
-			printf("zero");
+			printf ("zero");
 	}
-	else if (sign == 0 &&  exp == 255 && mantisa==0 ) 
+	else if (sign == 0 && exp == 255 && mantisa==0 ) 
 	{
-		printf(" + infinity ");
+		printf (" + infinity ");
 	}
 	else if (sign == -1 &&  exp == 255 && mantisa==0)
 	{
-		printf(" - infinity ");
+		printf (" - infinity ");
 	} 
 	else if (exp == 255 && mantisa != 0 )
 	{
-		printf("NaN");
+		printf ("NaN");
 	}
 	else
 	{
