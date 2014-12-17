3e61a063a718b5819234723f9cf6808fffac2541
Sharganov Artem <sharganov.artem@yandex.ru>
2014-12-17 10:08:28 +0400

    Create substring.c

---
+++ substring.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+	char str[1000];
+	char substr[1000];
+	int str_len = 0;
+	int sub_len = 0;
+	int p = 1;
+	int counter = 0;
+	int i,j;
+	scanf("%s", str);
+	scanf("%s", substr);
+	str_len = strlen(str);
+	sub_len = strlen(substr);
+    
+	for ( i = 0; i <= str_len - sub_len; i++)
+    { 
+		p = 1;
+		for (j = 0; j < sub_len; j++ )
+			{  
+				if (str[i+j] != substr[j])
+                {   
+					 p = 0;
+                    break;
+                }
+             }
+            if (p) counter++;
+            
+               
+        }
+    
+
+    printf("%d\n", counter);
+    return (0);
+
+}
