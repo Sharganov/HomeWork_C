bfe44e80d574a8de253d43becfa42b7cd629551a
Sharganov Artem <sharganov.artem@yandex.ru>
2014-12-17 15:22:37 +0400

    Update substring.c

--- substring.c
+++ substring.c
@@ -1,10 +1,16 @@
+/*
+Welcome to Storage
+To find substring in string
+By Sharganov Artem
+*/
+
 #include <stdio.h>
 #include <string.h>
 
 int main() 
 {
 	char str[1000];
-	char substr[1000];
+	char sub_str[1000];
 	int str_len = 0;
 	int sub_len = 0;
 	int p = 1;
@@ -13,14 +19,13 @@ int main()
 	scanf("%s", str);
 	scanf("%s", substr);
 	str_len = strlen(str);
-	sub_len = strlen(substr);
-    
+	sub_len = strlen(sub_str);
 	for ( i = 0; i <= str_len - sub_len; i++)
     	{ 
 		p = 1;
 		for (j = 0; j < sub_len; j++ )
 		{  
-			if (str[i+j] != substr[j])
+			if (str[i+j] != sub_str[j])
                 	{   
 				 p = 0;
                 		 break;
