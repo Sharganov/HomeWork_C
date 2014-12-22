4b9b2a23fb010d562fdb0aae9974490bab1bed75
Sharganov Artem <sharganov.artem@yandex.ru>
2014-12-22 23:20:31 +0400

    Library for longnumbers

---
+++ First semester/longnumlib.h
@@ -0,0 +1,14 @@
+/*
+Welcome to Storage
+Library for longnumbers
+By Sharganov Artem
+*/
+
+#include "listlib.h"
+
+void long_sum(node *result, node* first_number,node *second_number);
+void long_sub(node *result, node* first, node* second);
+void long_mult(node *result, node *first, node *second);
+void long_dev(node *result, node *first, node *second);
+void delete_num(node *head);
+void ln_delln(node* head);
