6a7fe8a686d39e1d9c520c1a65c3a45e26c58d44
Sharganov Artem <sharganov.artem@yandex.ru>
2014-12-24 01:01:45 +0400

    Update longnumlib.c

--- First semester/Calculator/longnumlib.c
+++ First semester/Calculator/longnumlib.c
@@ -4,6 +4,10 @@ Realisation of longnum.h
 By Sharganov Artem
 */
 
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 #include "longnumlib.h"
 
 void long_sum(node *result, node* first_number,node *second_number)
@@ -36,6 +40,8 @@ void long_sum(node *result, node* first_number,node *second_number)
 			lb_push(result, 1);
 		}
 	}
+	lb_delete(first_head);
+	lb_delete(second_head);
 }
 
 void long_sub(node *result, node* first, node* second)
@@ -54,7 +60,7 @@ void long_sub(node *result, node* first, node* second)
 	{	
 		if (f_lenght == 1 && first->val == 0) 
 		{
-			lb_insert(result, second->val);
+			lb_push(result, second->val);
 			second = second->next;
 			result_head->val = 1;
 		}
@@ -92,10 +98,14 @@ void long_sub(node *result, node* first, node* second)
 			second = second->next;
 		}
 	}
+	lb_delete(first_head);
+	lb_delete(second_head);
 }
 
 void long_mult(node *result, node *first, node *second)
 {
+	if(first->val == second->val) result->val = 0;
+	else result->val = 1;
 	node *first_head = first;
 	node *second_head = second;
 	first = first->next;
@@ -158,37 +168,53 @@ void long_mult(node *result, node *first, node *second)
 	tmp2 = tmp2_head;
 	lb_delete(tmp1);
 	lb_delete(tmp2);
+	lb_delete(first_head);
+	lb_delete(second_head);
 }
 
 void long_dev(node *result, node *first, node *second)
 {
-	node *tmp1 = lb_get_new(0);
-	node *tmp2 = lb_get_new(0);
-	unsigned int f_lenght = lb_lenght(first);;
 	unsigned int s_lenght = lb_lenght(second);
+	unsigned int f_lenght = lb_lenght(first);
+	node *tmp1 = NULL;
+	node *tmp2 = lb_get_new(0);
+	node *tmp3 = lb_get_new(0);
+	node *copy_second = lb_get_new(0);
+	copy(copy_second, second);
+	node *first_head = first;
+	node *second_head = second;
 	first = lb_reverse(first);
 	int res = 0;
+	if(s_lenght == 1 && second->next->val == 0) 
+	{
+		printf("Division by zero");
+		exit(1);
+	}
+	int k = 0;
 	if (f_lenght >= s_lenght)
 	{
 		while(s_lenght--)
 		{
-			lb_front_pop(tmp1, first);
+			lb_front_pop(tmp3, first);
 		}
-		
-
+				
 		while (1)
 		{
-			if(tmp2->val == 1) lb_front_pop(tmp1, first);
+			if(tmp2->val == 1) lb_front_pop(tmp3, first);
 			tmp2->val = 0;
 			while(1)
 			{
+				tmp1 = lb_get_new(0);
+				copy(tmp1, tmp3);
+				second = lb_get_new(0);
+				copy(second, copy_second);
 				long_sub(tmp2, tmp1, second);
 				if(tmp2->val == 1)
 				{
 					delete_num(tmp2);
 					break;
 				}
-				copy(tmp1, tmp2);
+				copy(tmp3, tmp2);
 				delete_num(tmp2);
 				res++;
 				
@@ -197,14 +223,42 @@ void long_dev(node *result, node *first, node *second)
 			res = 0;
 			if(first->next == NULL) break;
 		}
+		first->next = NULL;
 	}
-	first->next = NULL;
-	lb_delete(tmp1);
+		else
+		{
+			lb_push(result, 0);
+			lb_delete(second);
+		}
+	if(first->val == copy_second->val) result->val = 0;
+	else result->val = 1;
+
+	if(first->val == 1)
+	{
+		result = result->next;
+		int extra = 1;
+		while (extra == 1)
+		{
+			result->val = result->val + 1;
+			if(result->val < 10)
+				extra = 0;
+			else 
+			{
+				extra = 1;
+				result = result->next;
+			}
+		}
+	}
+	
+	lb_delete(first);
+	lb_delete(tmp3);
 	lb_delete(tmp2);
-}
+	lb_delete(copy_second);
+	}
 
 void delete_num(node *head)
 {
+	if(head == NULL) return;
 	if(head->next)
 	{
 	lb_delete(head->next);
@@ -214,12 +268,23 @@ void delete_num(node *head)
 
 void ln_delln(node *head)
 {
-	printf("3");
-	head = lb_reverse(head->next);
-	while (head->next && head->next->val == 0)
+	head = lb_reverse(head);
+	while (head->next->next && head->next->val == 0)
 	{
+		if(head->next->next == NULL) return;
 		lb_delete_element(head);	
 	}
-	printf("2");
-	head = lb_reverse(head->next);
+	head = lb_reverse(head);
+}
+
+void copy(node *where , node *from)
+{
+	egualize(where, from);
+	while(from)
+	{
+		where->val = from->val;
+		from = from->next;
+		where = where->next;
+	}
+
 }
