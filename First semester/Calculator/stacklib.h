a1d390b6baa6627e0cf54642e339a7ca8bc642a9
Sharganov Artem <sharganov.artem@yandex.ru>
2014-12-24 01:05:55 +0400

    Update stacklib.h

--- First semester/Calculator/stacklib.h
+++ First semester/Calculator/stacklib.h
@@ -13,10 +13,13 @@ typedef struct stack
 }stack;
 
 void st_print(stack *element);
-void st_push(stack *element, char arr[]);
+void st_print_head(stack *element);
+void st_push_char(stack *element, char arr[]);
+void st_push_ln(stack *element, node *longnum);
 void st_push_front(stack *element, node *longnum);
 stack* st_get_last(stack *head);
 stack* st_get_prelast(stack *element);
 void st_delete_element(stack *element);
 void st_delete_list(stack* head);
 stack* st_pop(stack* element);
+
